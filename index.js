51389510c4d966ebb21cfff99175b99db8c846b9
SUBHODEEP MODAK <134997844+subhodeepmodak@users.noreply.github.com>
2024-11-10 20:48:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,3 +19,22 @@ export const handler = async (event) => {
   };
   return response;
 };
+
+//Q.9] Lambda s3 bucket
+import json
+
+
+def lambda_handler(event, context):
+    # Extract bucket name and object key from the event
+    for record in event['Records']:
+        bucket_name = record['s3']['bucket']['name']
+        object_key = record['s3']['object']['key']
+       
+        # Log the message
+        print(f"An object has been added to the bucket {bucket_name}: {object_key}")
+
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Log completed')
+    }
